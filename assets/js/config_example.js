cf5fbcb9f5e4220287d4f6a965af59e81a9e1930
samuel <temmodaryl317@gmail.com>
2025-04-20 03:14:54 +0200

    Diversis

---
+++ assets/js/config_example.js
@@ -0,0 +1,7 @@
+// Configuration file to store EmailJS credentials, create a config.js and file the values with your own EmailJS credentials
+const CONFIG = {
+    EMAIL_JS_PUBLIC_KEY: "your_public_key_here",
+    EMAIL_JS_SERVICE_ID: "your_service_id_here",
+    EMAIL_JS_TEMPLATE_ID: "your_template_id_here"
+  };
+  
